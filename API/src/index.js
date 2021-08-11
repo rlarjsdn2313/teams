c97701853e608cd606c6fd34a7ec91d70e0a34a5
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-08-11 13:14:31 +0900

    add listening part

--- API/src/index.js
+++ API/src/index.js
@@ -1,2 +1,11 @@
+// import web framework lib
 const express = require('express');
 const app = express();
+
+// listening port
+const port = 3000;
+
+// listening
+app.listen(port, () => {
+    console.log(`Server is running on port ${ port }`);
+});
