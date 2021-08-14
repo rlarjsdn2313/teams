11c2ec7923bee78f56b05c3c89ff48ccca873883
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-08-14 19:06:11 +0900

    add checkStdntId

--- API/src/router/Auth.js
+++ API/src/router/Auth.js
@@ -4,6 +4,48 @@ var router = express.Router();
 // import sending mail object
 var mail = require('../lib/mail');
 
+
+// checkStdntId
+var checkStdntId = (stdntId) => {
+    // check stdntId lenth
+    if (stdntId.length !== 5) {
+        return 'too long';
+    }
+
+
+    // check string
+    let numbers = '1234567890';
+
+    var i = 0;
+    while (i < stdntId.length) {
+        if (!numbers.includes(stdntId[i])) {
+            return 'format type error';
+        }
+        i++;
+    }
+
+    let grade = stdntId[0];
+    if (!'123'.includes(grade)) {
+        return 'grade error';
+    }
+
+    let cl = stdntId[2];
+    if (!'123456789'.includes(cl)) {
+        return 'class error';
+    }
+
+    let num = parseInt(`${stdntId[3]}${stdntId[4]}`);
+    if (!(1 <= num <= 35)) {
+        return 'num error';
+    }
+
+    return '';
+}
+
+let createCode = (stdntId) => {
+    let code = `${Math.floor(Math.random() * (9999 - 0)) + 0}`.padStart(4, '0');
+    
+}
 /*
 {
     stdntId: 학번
@@ -15,8 +57,8 @@ router.post('/', (req, res) => {
     // check stdntId in reqKeyList
     if (!reqKeyList.includes('stdntId')) {
         res.send({
-            'error': true,
-            'message': 'request error'
+            error: true,
+            message: 'request error'
         });
 
         // router exit
@@ -25,6 +67,15 @@ router.post('/', (req, res) => {
 
     // get stdntId
     let stdntId = req.body.stdntId;
+    let check = checkStdntId(stdntId);
+
+    if (check.length != 0) {
+        res.send({
+            error: true,
+            message: check
+        });
+        return;
+    }
 
     let authCode = '213123'
 
@@ -36,8 +87,8 @@ router.post('/', (req, res) => {
     );
 
     res.send({
-        'error': false,
-        'message': 'Authorization email sent'
+        error: false,
+        message: 'Authorization email sent'
     });
 
     return;
